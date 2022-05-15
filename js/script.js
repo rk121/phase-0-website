7e246527cfa857910c4d2181b76cb6c35d77ff7b
rk121 <roshankhadka613@gmail.com>
2022-05-15 12:05:36 +1000

    Added navigation bar, script for scrolled feature and styled navigation bar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector("#navbar");
+window.onscroll = function() {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
+};
